92721998d96b830d65e3122d21a25d0eadde479c
Joakim Husmo <joakimhusmo@outlook.com>
2025-02-21 10:58:09 +0100

    Add randomChoiceComputer to index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("External JavaScript file.")
+
+const getComputerChoice = () => {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+};
+console.log(getComputerChoice());
+
